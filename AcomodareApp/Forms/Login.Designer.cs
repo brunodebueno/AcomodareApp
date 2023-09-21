910bd843238cc1e81c2795767d4ced51cdf63a5f
Bruno de Paula Bueno <67610168+brunodebueno@users.noreply.github.com>
2023-09-20 22:41:59 -0300

    Add Files

---
+++ AcomodareApp/Forms/Login.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AcomodareApp {
+    partial class Login {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pnlLoginContainer = new System.Windows.Forms.Panel();
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.pnlLoginContainer.SuspendLayout();
+            this.pnlLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLoginContainer
+            // 
+            this.pnlLoginContainer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlLoginContainer.BackColor = System.Drawing.Color.White;
+            this.pnlLoginContainer.Controls.Add(this.pnlLogin);
+            this.pnlLoginContainer.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlLoginContainer.Location = new System.Drawing.Point(340, 0);
+            this.pnlLoginContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlLoginContainer.MinimumSize = new System.Drawing.Size(340, 430);
+            this.pnlLoginContainer.Name = "pnlLoginContainer";
+            this.pnlLoginContainer.Size = new System.Drawing.Size(375, 430);
+            this.pnlLoginContainer.TabIndex = 6;
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Controls.Add(this.label4);
+            this.pnlLogin.Controls.Add(this.label3);
+            this.pnlLogin.Controls.Add(this.label2);
+            this.pnlLogin.Controls.Add(this.label1);
+            this.pnlLogin.Controls.Add(this.txtSenha);
+            this.pnlLogin.Controls.Add(this.txtUsuario);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.lblSenha);
+            this.pnlLogin.Location = new System.Drawing.Point(17, 33);
+            this.pnlLogin.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(340, 360);
+            this.pnlLogin.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(129)))), ((int)(((byte)(226)))));
+            this.label4.Location = new System.Drawing.Point(39, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "esqueci a senha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Faça seu login para entar no sistema";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(111)))), ((int)(((byte)(214)))));
+            this.label2.Location = new System.Drawing.Point(94, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 30);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "seja bem vindo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.label1.Location = new System.Drawing.Point(39, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Usuario";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSenha.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(39, 234);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(266, 33);
+            this.txtSenha.TabIndex = 1;
+            this.txtSenha.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSenha_KeyUp);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(39, 172);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(266, 33);
+            this.txtUsuario.TabIndex = 0;
+            this.txtUsuario.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtUsuario_KeyUp);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(129)))), ((int)(((byte)(226)))));
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(175)))), ((int)(((byte)(255)))));
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(64)))), ((int)(((byte)(149)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(42, 313);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(263, 35);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Acessar";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.lblSenha.Location = new System.Drawing.Point(39, 218);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(39, 13);
+            this.lblSenha.TabIndex = 11;
+            this.lblSenha.Text = "Senha";
+            // 
+            // background
+            // 
+            this.background.Image = global::AcomodareApp.Properties.Resources.login_background;
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Margin = new System.Windows.Forms.Padding(0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(295, 181);
+            this.background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background.TabIndex = 5;
+            this.background.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(715, 430);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlLoginContainer);
+            this.Controls.Add(this.background);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(715, 430);
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Login";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.Resize += new System.EventHandler(this.Login_Resize);
+            this.pnlLoginContainer.ResumeLayout(false);
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.Panel pnlLoginContainer;
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblSenha;
+    }
+}
---
+++ AcomodareApp/Forms/MDIPrincipal.cs
@@ -0,0 +1,199 @@
+﻿using AcomodareApp.Forms;
+using AcomodareApp.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace AcomodareApp {
+    public partial class MDIPrincipal : Form {
+
+        private const int cGrip = 16;
+        private const int cCaption = 32;        
+        private bool enableMoving = false;
+        private Point initialClickedPoint;
+
+        Login login;
+        Pets pets = new Pets();
+        private bool exibiuLogin = false;
+
+        public MDIPrincipal() {
+            InitializeComponent();
+        }
+        private void FMDIPrincipal_Load(object sender, EventArgs e) {
+            login.MdiParent = this;
+
+            pnlLateral.Hide();
+            login.Show();
+            login.Location = new Point(0, 0);
+            this.ActivateMdiChild(login);
+            this.ActiveMdiChild.Dock = DockStyle.Fill;
+
+            this.SetBevel(false);
+
+        }
+
+        private void MDIPrincipal_SizeChanged(object sender, EventArgs e) {
+            if (!exibiuLogin) {
+                exibiuLogin = true;
+                login = new Login();
+            }
+        }
+
+        #region Funções Locais
+        public void EfetuouLogin() {
+            pnlLateral.Show();
+            MaximizarForm();
+        }
+
+        private void DoubleClickRedimensionar() {
+            if (this.WindowState == FormWindowState.Normal)
+                MaximizarForm();
+            else if (this.WindowState == FormWindowState.Maximized)
+                WindowState = FormWindowState.Normal;
+        }
+
+        private void MaximizarForm() {
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        #endregion
+
+        #region Funções Redimensionamento Form
+        protected override void OnPaint(PaintEventArgs e) {
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+            rc = new Rectangle(0, 0, this.ClientSize.Width, cCaption);
+            e.Graphics.FillRectangle(Brushes.DarkBlue, rc);
+        }
+        protected override void WndProc(ref Message m) {
+            if (m.Msg == 0x84) {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption) {
+                    m.Result = (IntPtr)2;  // HTCAPTION
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip) {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+        #endregion
+
+        #region Navegação
+        private void btnClose_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e) {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void pnlNav_DoubleClick(object sender, EventArgs e) {
+            DoubleClickRedimensionar();
+        }
+
+        private void pnlNav_MouseDown(object sender, MouseEventArgs e) {
+            enableMoving = true;
+            initialClickedPoint = e.Location;
+        }
+
+        private void pnlNav_MouseUp(object sender, MouseEventArgs e) {
+            enableMoving = false;
+        }
+        
+        private void pnlNav_MouseMove(object sender, MouseEventArgs e) {
+            if (enableMoving){
+                this.Location = new Point(e.X + this.Left - initialClickedPoint.X,
+                        e.Y + this.Top - initialClickedPoint.Y);
+            }
+        }
+        #endregion
+
+        #region Botoes
+
+        #endregion
+
+        private void timerPrincipal_Tick(object sender, EventArgs e)
+        {
+            //lblHoras.Text = DateTime.Now.ToString("HH:mm");            
+        }
+
+        private void btnAnimais_Click(object sender, EventArgs e)
+        {
+            pets.MdiParent = this;
+            pets.Show();
+            pets.Location = new Point(0, 0);
+            this.SetBevel(false);
+        }
+
+        private void MDIPrincipal_Resize(object sender, EventArgs e)
+        {
+            if(this.ActiveMdiChild != null) this.ActiveMdiChild.Dock = DockStyle.Fill;
+        }
+    }
+
+    public static class MDIClientSupport {
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        private static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_CLIENTEDGE = 0x200;
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOZORDER = 0x0004;
+        private const uint SWP_NOREDRAW = 0x0008;
+        private const uint SWP_NOACTIVATE = 0x0010;
+        private const uint SWP_FRAMECHANGED = 0x0020;
+        private const uint SWP_SHOWWINDOW = 0x0040;
+        private const uint SWP_HIDEWINDOW = 0x0080;
+        private const uint SWP_NOCOPYBITS = 0x0100;
+        private const uint SWP_NOOWNERZORDER = 0x0200;
+        private const uint SWP_NOSENDCHANGING = 0x0400;
+
+        public static bool SetBevel(this Form form, bool show) {
+            foreach (Control c in form.Controls) {
+                MdiClient client = c as MdiClient;
+                if (client != null) {
+                    int windowLong = GetWindowLong(c.Handle, GWL_EXSTYLE);
+
+                    if (show) {
+                        windowLong |= WS_EX_CLIENTEDGE;
+                    } else {
+                        windowLong &= ~WS_EX_CLIENTEDGE;
+                    }
+
+                    SetWindowLong(c.Handle, GWL_EXSTYLE, windowLong);
+
+                    // Update the non-client area.
+                    SetWindowPos(client.Handle, IntPtr.Zero, 0, 0, 0, 0,
+                        SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER |
+                        SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
+
+
